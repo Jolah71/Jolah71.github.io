7c0a981c7325a2649f37b1f7d631c8851708b632
Jolah71 <79677442+Jolah71@users.noreply.github.com>
2025-07-14 02:35:17 +0000

    customize this message

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -35,10 +35,10 @@ $(function () {
 
 
     // TODO 2 - Create Platforms
-createPlatform(50,50,200,20);
-createPlatform(350,200,200,20);
-createPlatform(550,450,200,40);
-createPlatform(350,550,200,50);
+createPlatform(50,30,200,20);
+createPlatform(250,200,200,20);
+createPlatform(550,450,200,20);
+createPlatform(350,550,200,20);
 createPlatform(150,650,200,20);
 
 
